edb47d672459e699b2fb817d6ce73cdf6a4d9651
Sergei <1161441@gmail.com>
2019-10-19 13:24:58 +0200

    Added CRUD for categories

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -7,7 +7,19 @@
     <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
 
-    <link rel="stylesheet" href="css/admin.css">
+    <link rel="stylesheet" href="/css/admin.css">
+    <style>
+        table.table form {
+            display: inline-block;
+        }
+
+        button.delete {
+            background: transparent;
+            border: none;
+            color: #337ab7;
+            padding: 0px;
+        }
+    </style>
 </head>
 <body class="hold-transition skin-blue sidebar-mini">
 <!-- Site wrapper -->
@@ -431,7 +443,7 @@
     <div class="control-sidebar-bg"></div>
 </div>
 
-<script src="js/admin.js"></script>
+<script src="/js/admin.js"></script>
 </body>
 
 </html>
