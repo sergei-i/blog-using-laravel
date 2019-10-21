c9135140d0fbef8e3baab213fb377feb5cfbf6e2
Sergei <1161441@gmail.com>
2019-10-21 15:55:26 +0200

    Added post pages

--- resources/views/pages/show.blade.php
+++ resources/views/pages/show.blade.php
@@ -7,48 +7,160 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-8">
-                    @foreach($posts as $post)
-                        <article class="post">
-                            <div class="post-thumb">
-                                <a href="{{ route('post.show', $post->slug) }}"><img src="{{ $post->getImage() }}" alt=""></a>
+                    <article class="post">
+                        <div class="post-thumb">
+                            <a href="blog.html"><img src="{{ $post->getImage() }}" alt=""></a>
+                        </div>
+                        <div class="post-content">
+                            <header class="entry-header text-center text-uppercase">
+                                <h6><a href="#"> {{ $post->getCategoryTitle() }}</a></h6>
 
-                                <a href="{{ route('post.show', $post->slug) }}" class="post-thumb-overlay text-center">
-                                    <div class="text-uppercase text-center">View Post</div>
-                                </a>
+                                <h1 class="entry-title"><a href="blog.html">{{ $post->title }}</a></h1>
+
+
+                            </header>
+                            <div class="entry-content">
+                                {!! $post->content !!}
                             </div>
-                            <div class="post-content">
-                                <header class="entry-header text-center text-uppercase">
-                                    <h6><a href="#"> {{ $post->category->title }}</a></h6>
+                            <div class="decoration">
+                                @foreach($post->tags as $tag)
+                                    <a href="#" class="btn btn-default">{{ $tag->title }}</a>
+                                @endforeach
+                            </div>
 
-                                    <h1 class="entry-title"><a href="{{ route('post.show', $post->slug) }}">{{ $post->title }}</a></h1>
+                            <div class="social-share">
+							<span
+                                class="social-share-title pull-left text-capitalize">By Rubel On {{ $post->getDate() }}
+                            </span>
+                                <ul class="text-center pull-right">
+                                    <li><a class="s-facebook" href="#"><i class="fa fa-facebook"></i></a></li>
+                                    <li><a class="s-twitter" href="#"><i class="fa fa-twitter"></i></a></li>
+                                    <li><a class="s-google-plus" href="#"><i class="fa fa-google-plus"></i></a></li>
+                                    <li><a class="s-linkedin" href="#"><i class="fa fa-linkedin"></i></a></li>
+                                    <li><a class="s-instagram" href="#"><i class="fa fa-instagram"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </article>
+                    <div class="top-comment"><!--top comment-->
+                        <img src="/images/comment.jpg" class="pull-left img-circle" alt="">
+                        <h4>Rubel Miah</h4>
+
+                        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy hello ro mod tempor
+                            invidunt ut labore et dolore magna aliquyam erat.</p>
+                    </div><!--top comment end-->
+                    <div class="row"><!--blog next previous-->
+                        <div class="col-md-6">
+                            @if($post->hasPrevious())
+                                <div class="single-blog-box">
+                                    <a href="{{ route('post.show', $post->getPrevious()->slug) }}">
+                                        <img src="{{ $post->getPrevious()->getImage() }}" alt="">
+
+                                        <div class="overlay">
+
+                                            <div class="promo-text">
+                                                <p><i class=" pull-left fa fa-angle-left"></i></p>
+                                                <h5>{{ $post->getPrevious()->title }}</h5>
+                                            </div>
+                                        </div>
 
 
-                                </header>
-                                <div class="entry-content">
-                                    <p>{!! $post->description !!}</p>
-                                    <div class="btn-continue-reading text-center text-uppercase">
-                                        <a href="{{ route('post.show', $post->slug) }}" class="more-link">Continue Reading</a>
-                                    </div>
+                                    </a>
                                 </div>
-                                <div class="social-share">
-                                    <span class="social-share-title pull-left text-capitalize">By
-                                        <a href="#">Rubel</a> On {{ $post->getDate() }}</span>
-                                    <ul class="text-center pull-right">
-                                        <li><a class="s-facebook" href="#"><i class="fa fa-facebook"></i></a></li>
-                                        <li><a class="s-twitter" href="#"><i class="fa fa-twitter"></i></a></li>
-                                        <li><a class="s-google-plus" href="#"><i class="fa fa-google-plus"></i></a></li>
-                                        <li><a class="s-linkedin" href="#"><i class="fa fa-linkedin"></i></a></li>
-                                        <li><a class="s-instagram" href="#"><i class="fa fa-instagram"></i></a></li>
-                                    </ul>
+                            @endif
+                        </div>
+                        <div class="col-md-6">
+                            @if($post->hasNext())
+                                <div class="single-blog-box">
+                                    <a href="{{ route('post.show', $post->getNext()->slug) }}">
+                                        <img src="{{ $post->getNext()->getImage() }}" alt="">
+
+                                        <div class="overlay">
+                                            <div class="promo-text">
+                                                <p><i class=" pull-right fa fa-angle-right"></i></p>
+                                                <h5>{{ $post->getNext()->title }}</h5>
+
+                                            </div>
+                                        </div>
+                                    </a>
+                                </div>
+                            @endif
+                        </div>
+                    </div><!--blog next previous end-->
+                    <div class="related-post-carousel"><!--related post carousel-->
+                        <div class="related-heading">
+                            <h4>You might also like</h4>
+                        </div>
+                        <div class="items">
+                            @foreach($post->related() as $item)
+                                <div class="single-item">
+                                    <a href="{{ route('post.show', $item->slug) }}">
+                                        <img src="{{ $item->getImage() }}" alt="">
+
+                                        <p>{{ $item->title }}</p>
+                                    </a>
+                                </div>
+                            @endforeach
+                        </div>
+                    </div><!--related post carousel-->
+                    <div class="bottom-comment"><!--bottom comment-->
+                        <h4>3 comments</h4>
+
+                        <div class="comment-img">
+                            <img class="img-circle" src="/images/comment-img.jpg" alt="">
+                        </div>
+
+                        <div class="comment-text">
+                            <a href="#" class="replay btn pull-right"> Replay</a>
+                            <h5>Rubel Miah</h5>
+
+                            <p class="comment-date">
+                                December, 02, 2015 at 5:57 PM
+                            </p>
+
+
+                            <p class="para">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+                                diam nonumy
+                                eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
+                                voluptua. At vero eos et cusam et justo duo dolores et ea rebum.</p>
+                        </div>
+                    </div>
+                    <!-- end bottom comment-->
+
+
+                    <div class="leave-comment"><!--leave comment-->
+                        <h4>Leave a reply</h4>
+
+
+                        <form class="form-horizontal contact-form" role="form" method="post" action="#">
+                            <div class="form-group">
+                                <div class="col-md-6">
+                                    <input type="text" class="form-control" id="name" name="name" placeholder="Name">
+                                </div>
+                                <div class="col-md-6">
+                                    <input type="email" class="form-control" id="email" name="email"
+                                           placeholder="Email">
                                 </div>
                             </div>
-                        </article>
-                    @endforeach
-                    {{ $posts->links() }}
+
+                            <div class="form-group">
+                                <div class="col-md-12">
+                                    <input type="text" class="form-control" id="subject" name="subject"
+                                           placeholder="Website url">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <div class="col-md-12">
+										<textarea class="form-control" rows="6" name="message"
+                                                  placeholder="Write Massage"></textarea>
+                                </div>
+                            </div>
+                            <a href="#" class="btn send-btn">Post Comment</a>
+                        </form>
+                    </div><!--end leave comment-->
                 </div>
                 <div class="col-md-4" data-sticky_column>
                     <div class="primary-sidebar">
-
                         <aside class="widget news-letter">
                             <h3 class="widget-title text-uppercase text-center">Get Newsletter</h3>
 
@@ -146,10 +258,10 @@
 
                             <div class="thumb-latest-posts">
 
-
                                 <div class="media">
                                     <div class="media-left">
                                         <a href="#" class="popular-img"><img src="/images/r-p.jpg" alt="">
+
                                             <div class="p-overlay"></div>
                                         </a>
                                     </div>
@@ -165,6 +277,7 @@
                                 <div class="media">
                                     <div class="media-left">
                                         <a href="#" class="popular-img"><img src="/images/r-p.jpg" alt="">
+
                                             <div class="p-overlay"></div>
                                         </a>
                                     </div>
@@ -180,6 +293,7 @@
                                 <div class="media">
                                     <div class="media-left">
                                         <a href="#" class="popular-img"><img src="/images/r-p.jpg" alt="">
+
                                             <div class="p-overlay"></div>
                                         </a>
                                     </div>
@@ -195,6 +309,7 @@
                                 <div class="media">
                                     <div class="media-left">
                                         <a href="#" class="popular-img"><img src="/images/r-p.jpg" alt="">
+
                                             <div class="p-overlay"></div>
                                         </a>
                                     </div>
@@ -234,46 +349,46 @@
                                 </li>
                             </ul>
                         </aside>
+                        <aside class="widget pos-padding">
+                            <h3 class="widget-title text-uppercase text-center">Follow@Instagram</h3>
+
+                            <div class="instragram-follow">
+                                <a href="#">
+                                    <img src="/images/ins-flow.jpg" alt="">
+                                </a>
+                                <a href="#">
+                                    <img src="/images/ins-flow.jpg" alt="">
+                                </a>
+                                <a href="#">
+                                    <img src="/images/ins-flow.jpg" alt="">
+                                </a>
+                                <a href="#">
+                                    <img src="/images/ins-flow.jpg" alt="">
+                                </a>
+                                <a href="#">
+                                    <img src="/images/ins-flow.jpg" alt="">
+                                </a>
+                                <a href="#">
+                                    <img src="/images/ins-flow.jpg" alt="">
+                                </a>
+                                <a href="#">
+                                    <img src="/images/ins-flow.jpg" alt="">
+                                </a>
+                                <a href="#">
+                                    <img src="/images/ins-flow.jpg" alt="">
+                                </a>
+                                <a href="#">
+                                    <img src="/images/ins-flow.jpg" alt="">
+                                </a>
+
+                            </div>
+
+                        </aside>
                     </div>
                 </div>
             </div>
         </div>
     </div>
     <!-- end main content-->
-    <!--footer start-->
-    <div id="footer">
-        <div class="footer-instagram-section">
-            <h3 class="footer-instagram-title text-center text-uppercase">Instagram</h3>
-
-            <div id="footer-instagram" class="owl-carousel">
-
-                <div class="item">
-                    <a href="#"><img src="/images/ins-1.jpg" alt=""></a>
-                </div>
-                <div class="item">
-                    <a href="#"><img src="/images/ins-2.jpg" alt=""></a>
-                </div>
-                <div class="item">
-                    <a href="#"><img src="/images/ins-3.jpg" alt=""></a>
-                </div>
-                <div class="item">
-                    <a href="#"><img src="/images/ins-4.jpg" alt=""></a>
-                </div>
-                <div class="item">
-                    <a href="#"><img src="/images/ins-5.jpg" alt=""></a>
-                </div>
-                <div class="item">
-                    <a href="#"><img src="/images/ins-6.jpg" alt=""></a>
-                </div>
-                <div class="item">
-                    <a href="#"><img src="/images/ins-7.jpg" alt=""></a>
-                </div>
-                <div class="item">
-                    <a href="#"><img src="/images/ins-8.jpg" alt=""></a>
-                </div>
-
-            </div>
-        </div>
-    </div>
 
 @endsection
